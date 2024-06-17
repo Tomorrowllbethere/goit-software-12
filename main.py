ba294c8ca4aef6d70b0e9ec9f317e707ef20948b
Slytherin fantom <uliadiakiv@gmail.com>
2024-06-17 23:31:36 +0200

    fastapi/auth
    realize: version with authorization and verification users

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+from str.routes import notes, auth
+
+app = FastAPI()
+
+app.include_router(notes.router, prefix='/api')
+app.include_router(auth.router, prefix='/api')
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ str/schemas.py
@@ -0,0 +1,48 @@
+# схеми. валідація вхідних і вихідних  даних
+from datetime import date, datetime
+from typing import List, Optional
+from pydantic import BaseModel, Field, EmailStr, constr
+import re
+
+class ContactBase(BaseModel):
+    first_name: str = Field(max_length=50)
+    last_name:str = Field(max_length=50)
+    email: EmailStr
+    phone_number:str = Field(pattern=r'^\+?1?\d{9,15}$')
+    date_of_birth: date
+    
+class ContactModel(ContactBase):
+    info: Optional[constr(max_length=350)]
+    
+class ContactResponse(ContactBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class UserModel(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: str
+    password: str = Field(min_length=6, max_length=10)
+
+
+class UserDb(BaseModel):
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+    avatar: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(BaseModel):
+    user: UserDb
+    detail: str = "User successfully created"
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
